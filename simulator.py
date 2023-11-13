888e3610ffae2e098dc86ed2f43a338eaba78809
chawrewerwee <paweekorn.pb@gmail.com>
2023-11-14 01:08:39 +0700

    add motor

--- simulator.py
+++ simulator.py
@@ -14,16 +14,16 @@ I2 = 0.0027 # Innertia moment of Wheel (Kg*m^2)
 g = 9.81 # Gravitational acceleration
 
 # Motor param
-kt = 0.0649 # Mechanical constant of DC Motor (Nm/A)
-ke = 0.0649 # Electrical constant of DC Motor (Vs/rad)
-Ng = 1 # Transmission ratio of DC Motor
-R = 6.83 # Resistor of DC Motor (Ohm)
-
-a = m1*(L1**2) + m2*(L2**2) + I1
-b = (m1*L1 + m2*L2) * g
+J = 1.7e-6 # Innertia Motor (Kgm^2)
+Ng = 0.83 # Transmission ratio of DC Motor
+ke = 3.69e-2 # Electrical constant of DC Motor (Vs/rad)
+kt = ke * Ng # Mechanical constant of DC Motor (Nm/A)
+R = 2.85 # Resistor of DC Motor (Ohm)
+L = 3.73e-4 # Inductance of DC Motor (Henry)
+B = 3.85e-6 # Damped of DC Motor (Nn/v)
 
 def qp_dd_cal(q, Tr):
-    qdd = ((m1*g*L1*math.sin(q)) + (m2*g*L2*math.sin(q)) - Tr)/((m1*L1**2) + (m2*L2) + I1)
+    qdd = ((m1*g*L1*math.sin(q)) + (m2*g*L2*math.sin(q)) - Tr)/((m1*L1**2.0) + (m2*L2) + I1)
     return qdd
 
 def Forwardkinematics(q):
@@ -32,42 +32,57 @@ def Forwardkinematics(q):
     return x, y
 
 def PendulumEnergy(q,qd):
-    K = 0.5*m1*(qd*L1)**2 # Kinetic energy
+    K = 0.5*m1*(qd*L1)**2.0 # Kinetic energy
     P = (m1 + m2)*g*L2*math.cos(q) # Potential energy
     return K + P
+
+qr_d = 0
+def MotorDynamics(Vin, dt):
+    global qr_d
+    Tm = ((Vin - (qr_d*ke))/R) * kt
+    qr_dd = (Tm - B*qr_d)/J
+    qr_d = qr_d + (qr_dd * dt)
+    return Tm
 
 qp = math.pi # initial pendulum angle
 qp_d = 0.0 # initial pendulum spe
 
-Tr = 1.0
+Tr = 1
 
 timestamp = 0
-dt = 0.01
+dt = 0.0001
 fig, ax = plt.subplots()
 
 ax.grid(True)
 
+timep = 0
+
 while True:
     if time.time() - timestamp >= dt:
         timestamp = time.time()
-        T = PendulumEnergy(qp, qp_d)
-        if T < (m1 + m2)*g*L2*math.cos(0) and qp >= math.pi and qp_d < 0.01:
-            Tr = 1
-        else:
-            Tr = 0
+        # T = PendulumEnergy(qp, qp_d)
+        # if T < (m1 + m2)*g*L2*math.cos(0) and qp >= math.pi and qp_d < 0.01:
+        #     Tr = 1
+        # else:
+        #     Tr = 0
 
-        qp_dd = qp_dd_cal(qp,Tr)
-        qp_d = qp_d + (qp_dd * dt)
-        qp = qp + (qp_d * dt)
+        Tm = MotorDynamics(24,dt)
+        timep += dt
+        print(Tm,qr_d)
+        # plt.plot(timep, qr_dp, label='qr_d')
 
-        x, y = Forwardkinematics(qp)
+        # qp_dd = qp_dd_cal(qp,Tr)
+        # qp_d = qp_d + (qp_dd * dt)
+        # qp = qp + (qp_d * dt)
 
-        print(qp)
-        ax.clear()
-        ax.plot([0, x], 
-                [0, y], 'o-')  # Plot robot
+        # x, y = Forwardkinematics(qp)
 
-        ax.set_xlim(-2*L1, 2*L1)
-        ax.set_ylim(-2*L1, 2*L1)
-        ax.set_aspect('equal')
+        # print(qp)
+        # ax.clear()
+        # ax.plot([0, x], 
+        #         [0, y], 'o-')  # Plot robot
+
+        # ax.set_xlim(-2*L1, 2*L1)
+        # ax.set_ylim(-2*L1, 2*L1)
+        # ax.set_aspect('equal')
         plt.pause(dt)
